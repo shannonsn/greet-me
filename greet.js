97de0ea4e7b052dc2b2496ceab1a0e887ddffe4f
Shannon Nichols <shannonsn19@gmail.com>
2017-03-08 15:03:50 +0200

    wrote a function to greet the same name and display the same language without incrementing

--- greet.js
+++ greet.js
@@ -7,46 +7,52 @@ if (localStorage.getItem('counter') === null) {
     document.getElementById("counter").innerHTML = localStorage.getItem("counter");
 }
 var namesGreeted = {};
-//console.log(namesGreeted);
 document.getElementById("myText").innerHTML
 for (var i = 0; i < namesGreeted.length; i++) {
-    //console.log(namesGreeted[i])
+
 }
-//if (namesGreeted[x] === undefined) {
-//    greetingsCounter++;
-//}
+
 
 function myFunction() {
     var myTextString = "myText";
-    var x = document.getElementById(myTextString).value;
+    var x = document.getElementById("myText").value;
 
     var radioButtonEng = document.getElementById('english');
 
-    if (radioButtonEng.checked && x.length > 0 && namesGreeted[x] === undefined) {
-        namesGreeted[x]=1
+    if (radioButtonEng.checked && x.length > 0 && x !== " " && namesGreeted[x] === undefined) {
+        namesGreeted[x] = 1
         document.getElementById('demo').innerHTML = 'Hello, ' + x;
         counter++;
         localStorage.setItem('counter', counter)
         document.getElementById('counter').innerHTML = counter;
         namesGreeted.push(document.getElementById("myText").value);
-    } else if (document.getElementById('espanol').checked && x.length > 0 && namesGreeted[x] === undefined) {
-      namesGreeted[x]=1
+    } else if (document.getElementById('espanol').checked && x.length > 0 && x !== " " && namesGreeted[x] === undefined) {
+        namesGreeted[x] = 1
         document.getElementById('demo').innerHTML = 'Hola, ' + x;
         counter++;
         localStorage.setItem('counter', counter)
         document.getElementById('counter').innerHTML = counter
         namesGreeted.push(document.getElementById("myText").value);
-    } else if (document.getElementById('dutch').checked && x.length > 0 && namesGreeted[x] === undefined) {
-      namesGreeted[x]=1
+    } else if (document.getElementById('dutch').checked && x.length > 0 && x !== " " && namesGreeted[x] === undefined) {
+        namesGreeted[x] = 1
         document.getElementById('demo').innerHTML = 'Hallo, ' + x;
         counter++;
         localStorage.setItem('counter', counter)
         document.getElementById('counter').innerHTML = counter;
         namesGreeted.push(document.getElementById("myText").value);
+    } else if (document.getElementById("english").checked && x.length > 0 && x !== " " && x !== undefined) {
+      document.getElementById("demo").innerHTML = 'Hello, ' + x;
+       document.getElementById("counter").innerHTML = counter;
+    } else if (document.getElementById("espanol").checked && x.length > 0 && x !== " " && x !== undefined) {
+        document.getElementById("demo").innerHTML = 'Hola ' + x;
+        document.getElementById("counter").innerHTML = counter;
+    } else if (document.getElementById("dutch").checked && x.length > 0 && x !== " " && x !== undefined) {
+        document.getElementById("demo").innerHTML = 'Hallo ' + x;
+        document.getElementById("counter").innerHTML = counter;
     }
+
     document.getElementById('myText').value = '';
 }
-
 
 function myResetFunction() {
     var reset = 0

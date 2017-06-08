95eddad1f8b55a83ba27b388368b1a7c9b55894e
Shannon Nichols <shannonsn19@gmail.com>
2017-06-08 12:08:21 +0200

    removed unnecessary comments

--- getInputName.js
+++ getInputName.js
@@ -1,6 +1,4 @@
-// var inputValue = document.getElementById("myText").value;
 // this function gets the name from the textbox
-// var namesGreeted = [];
 function getInputName() {
  var inputValue = document.getElementById("myText").value;
  return inputValue;
